06259de93c6c268792a306f04a9e7007ba61ff88
pweege <34147630+pweege@users.noreply.github.com>
2018-03-27 15:41:58 -0300

    Update automacao_residencial.js

--- automacao_residencial.js
+++ automacao_residencial.js
@@ -3,11 +3,11 @@ function AlteraRele1(){
     if(estado === "0"){
         document.getElementById("estado").innerHTML="1";
         document.getElementById("rele").innerHTML="<div class='rele1_ligado'></div>";
-		document.getElementById("botao").innerHTML="<a href='/?desligar' class='botao'>Desliga</a>";
+		document.getElementById("botao").innerHTML="<a href='/H' class='botao'>Desliga</a>";
     } else {
         document.getElementById("estado").innerHTML="0";
         document.getElementById("rele").innerHTML="<div class='rele1_desligado'></div>";
-        document.getElementById("botao").innerHTML="<a href='/?ligar' class='botao'>Liga</a>";
+        document.getElementById("botao").innerHTML="<a href='/L' class='botao'>Liga</a>";
     }
 }
 
